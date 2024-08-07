7e7ee7c6cf0fbd20d6e34d527a3b768540bb758c
Saipraneeth Mukku <saipmukku@gmail.com>
2024-08-07 17:57:36 -0400

    Big update to home page animations, layout, and pictures

--- site/src/App.js
+++ site/src/App.js
@@ -24,17 +24,45 @@ function GamePage({thumbnail, info, links, descList}) {
 }
 
 function Home() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
   return (
-    <div className="App-header">
-      <div className="header">
-        <img id="logo-picture" src="/pngs/zeronialogo-2021.png" alt="logo"/>
-        <span className="snowstorm" id="logo-text">Zeronia</span>
-        <div className="tabs">
-          <span>Home</span>
-          <span>Announcements</span>
-          <span>Map Gallery</span>
-          <span>About</span>
+    <div className="app">
+      <div className="collage">
+        <div className="first-three">
+          <img className="back-pic"  src="/pngs/background-pic-1.png" alt="background-pic-1"/>
+          <img className="back-pic"  src="/pngs/background-pic-2.png" alt="background-pic-2"/>
+          <img className="back-pic"  src="/pngs/background-pic-3.png" alt="background-pic-3"/>
         </div>
+        <img className="clouds" src="/pngs/cloud-pixel-art.png"
+        alt="cloud-scroll-handling" style={{marginTop:`${700-scrollY}px`}}/>
+      </div>
+      <div className="top-bar">
+        <div id="logo">
+          <img id = "logo-icon" src="/pngs/zeronialogo-2021.png" alt="logo"/>
+          <h1 id="logo-text">Zeronia</h1>
+        </div>
+        <ul className="nav-bar">
+          <label className="top-text" href="default.asp">Home</label>
+          <label className="top-text" href="default.asp">Announcements</label>
+          <label className="top-text" href="default.asp">Minigames</label>
+          <label className="top-text" href="default.asp">About</label>
+        </ul>
+      </div>
+      <div className="main-body">
+        <h1 id="welcome-text">We Are Zeronia</h1>
       </div>
     </div>
   );
@@ -52,7 +80,8 @@ function App() {
 
       for (let i = 0; i < descList.length; i++) {
         let str = "/" + descList[i]["name"].replace(" ", "_")
-        routeList.push(<Route key = {i} path={str} element={<GamePage info={descList[i]} thumbnail={data["thumbnails"][i]} links={linkPaths} descList={descList}/>} />);
+        routeList.push(<Route key = {i} path={str} element={<GamePage info={descList[i]} 
+          thumbnail={data["thumbnails"][i]} links={linkPaths} descList={descList}/>} />);
         linkPaths.push(str);
       }
 
@@ -70,5 +99,7 @@ function App() {
     </BrowserRouter>
   );
 }
+
+// Helper functions
 
 export default App;
